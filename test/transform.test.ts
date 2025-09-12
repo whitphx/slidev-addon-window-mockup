6a31b67091fe90ae58514fbd9045e239de65a309
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-09-12 21:28:08 +0900

    Add tests

--- test/transform.test.ts
+++ test/transform.test.ts
@@ -28,6 +28,19 @@ console.log('Hello, World!');
   expect(ctx.s.toString()).toMatchSnapshot();
 });
 
+test("language name + ranges + options + window", () => {
+  const ctx = createTransformContext(`
+\`\`\`js {*|1|2}{at: 1} window
+console.log('Hello, World!');
+\`\`\`
+`);
+
+  transformWindowedCodeblock(ctx);
+  ctx.s.commit();
+
+  expect(ctx.s.toString()).toMatchSnapshot();
+});
+
 test("language name + ranges + options + window + windowOptions", () => {
   const ctx = createTransformContext(`
 \`\`\`js {*|1|2}{'maxHeight': '100px'} window {'color': 'dark'}
